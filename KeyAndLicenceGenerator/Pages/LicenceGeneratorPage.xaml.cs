d32b4148e20f741f6b5df1e77ec248fb032ceac0
hwarang098tkd <hwarang098tkd@gmail.com>
2024-04-25 21:28:53 +0300

    save

--- KeyAndLicenceGenerator/Pages/LicenceGeneratorPage.xaml.cs
+++ KeyAndLicenceGenerator/Pages/LicenceGeneratorPage.xaml.cs
@@ -1,3 +1,4 @@
+using KeyAndLicenceGenerator.Services;
 using KeyAndLicenceGenerator.ViewModels;
 using System.Diagnostics;
 
@@ -19,23 +20,5 @@ public partial class LicenceGeneratorPage : ContentPage
     private async void LicenceClicked(object sender, EventArgs e)
     {
         Debug.WriteLine("Licence Button Clicked");
-
-        // Only execute this on Windows
-#if WINDOWS
-        Debug.WriteLine("Licence Button Clicked1");
-        ShowWindowsToast("Licence Notification", "This is a toast notification for the Licence operation.");
-#endif
     }
-
-#if WINDOWS
-    private void ShowWindowsToast(string title, string content)
-    {
-        // Make sure to use the correct namespaces and setup for toast notifications as discussed earlier.
-        new Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder()
-            .AddText(title)
-            .AddText(content)
-            .Show();
-    }
-#endif
-
 }
