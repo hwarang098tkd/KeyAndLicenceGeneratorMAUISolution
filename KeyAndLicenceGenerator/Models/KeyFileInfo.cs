dd31793e6f0d40b134edd76fe3e7edd800278723
hwarang098tkd <hwarang098tkd@gmail.com>
2024-04-28 03:08:55 +0300

    save

--- KeyAndLicenceGenerator/Models/KeyFileInfo.cs
+++ KeyAndLicenceGenerator/Models/KeyFileInfo.cs
@@ -1,6 +1,6 @@
 ﻿namespace KeyAndLicenceGenerator.Models
 {
-    public class KeyFileInfo
+    public class CerFileInfo
     {
         public string FileName { get; set; }
         public string FilePath { get; set; }
@@ -8,6 +8,30 @@
         public string CommonName { get; set; }
         public string Email { get; set; }
         public DateTime CreationDate { get; set; }
-        public string KeyType { get; set; }
+    }
+
+    public class PfxFileInfo
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string CommonName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string KeyType { get; set; } // Specific to PFX files
+    }
+
+    public class LicenseFileInfo
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+    public class CertificatePairFileInfo
+    {
+        public CerFileInfo CerFile { get; set; }
+        public PfxFileInfo PfxFile { get; set; }
     }
 }
